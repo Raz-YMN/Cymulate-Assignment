748a85dc047d45635371797dd72f84a821e86adf
Raz <razyamin75@gmail.com>
2025-11-14 13:23:32 +0200

    Added custom metrics

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import logging
+import time
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Response
 import requests
 import os
 
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from logger import init_logger
+from metrics import *
 
 init_logger()
 logger = logging.getLogger()
@@ -12,26 +15,41 @@ logger = logging.getLogger()
 app = FastAPI()
 
 supported_currencies = ["usd", "ils"]
+supported_coins = ["bitcoin"]
 
 CURRENCY = os.getenv("API_CURRENCY", "usd").lower()
 if CURRENCY not in supported_currencies:
     logger.warning("Non-supported currency, defaulting to USD")
     CURRENCY = "usd"
 
+COIN_TYPE = os.getenv("API_COIN_TYPE", "bitcoin").lower()
+if COIN_TYPE not in supported_coins:
+    logger.warning("Non-supported coin, defaulting to bitcoin")
+    COIN_TYPE = "bitcoin"
+
 BASE_URL = os.getenv("API_URL", f"https://api.coingecko.com/api/v3/simple/price")
 
 @app.get("/price")
-def get_crypto_price(crypto: str = Query("bitcoin", description="Cryptocurrency name"),
-                     currency: str = Query("usd", description="Currency type")):
+def get_crypto_price(crypto: str = Query(COIN_TYPE, description="Cryptocurrency name"),
+                     currency: str = Query(CURRENCY, description="Currency type")):
     params = {
     "ids": crypto,
     "vs_currencies": currency
     }
+    start = time.time()
     response = requests.get(BASE_URL, params=params)
     response.raise_for_status()
     data = response.json()
 
     if crypto not in data:
+        increase_no_price(BASE_URL)
+        record_latency(BASE_URL, COIN_TYPE, start)
         return {"error": f"Price not found for '{crypto}'"}
 
+    increase_request_count(BASE_URL, crypto)
+    record_latency(BASE_URL, COIN_TYPE, start)
     return {"crypto": crypto, "price": data[crypto][currency.lower()]}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
---
+++ metrics.py
@@ -0,0 +1,38 @@
+from prometheus_client import Counter, Histogram
+import time
+
+request_count = Counter(
+    "crypto_price_requests_total",
+    "Total number of requests to /price for a specific coin",
+    ["endpoint", "coin_type"]
+)
+
+price_error_count = Counter(
+    "crypto_price_not_found_error_total",
+    "Total number of fails regarding finding price of coin",
+    ["endpoint"]
+)
+
+target_site_api_error_count = Counter(
+    "endpoint_api_error_total",
+    "Total number of errors trying to request data from target site",
+    ["endpoint"]
+)
+
+target_site_request_latency = Histogram(
+    "endpoint_api_request_latency_seconds",
+    "Latency of requests to /price endpoint",
+    ["endpoint", "coin_type"]
+)
+
+def increase_request_count(endpoint: str, coin_type: str):
+    request_count.labels(endpoint=endpoint, coin_type=coin_type).inc()
+
+def increase_no_price(endpoint: str):
+    price_error_count.labels(endpoint=endpoint).inc()
+
+def increase_site_api_error(endpoint: str):
+    target_site_api_error_count.labels(endpoint=endpoint).inc()
+
+def record_latency(endpoint: str, coin_type: str, start):
+    target_site_request_latency.labels(endpoint=endpoint, coin_type=coin_type).observe(time.time() - start)
