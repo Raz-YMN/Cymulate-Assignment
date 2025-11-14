00ce09ecd1ad9f05a1e35e41ac4fdc60790f1710
Raz-YMN <razyamin75@gmail.com>
2025-11-14 10:51:52 +0200

    Feat: Add main API functionality and Dockerfile (#1)
    * Added main functionally for getting crypto prices
    
    * Added dockerfile
    
    * Added logger
    
    * Change env var names

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+
+from fastapi import FastAPI, Query
+import requests
+import os
+
+from logger import init_logger
+
+init_logger()
+logger = logging.getLogger()
+
+app = FastAPI()
+
+supported_currencies = ["usd", "ils"]
+
+CURRENCY = os.getenv("API_CURRENCY", "usd").lower()
+if CURRENCY not in supported_currencies:
+    logger.warning("Non-supported currency, defaulting to USD")
+    CURRENCY = "usd"
+
+BASE_URL = os.getenv("API_URL", f"https://api.coingecko.com/api/v3/simple/price")
+
+@app.get("/price")
+def get_crypto_price(crypto: str = Query("bitcoin", description="Cryptocurrency name"),
+                     currency: str = Query("usd", description="Currency type")):
+    params = {
+    "ids": crypto,
+    "vs_currencies": currency
+    }
+    response = requests.get(BASE_URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    if crypto not in data:
+        return {"error": f"Price not found for '{crypto}'"}
+
+    return {"crypto": crypto, "price": data[crypto][currency.lower()]}
