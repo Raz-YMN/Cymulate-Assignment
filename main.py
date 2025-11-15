e88ab687d0b9374bbf4bf98699d3c3ad730b24f2
Raz-YMN <razyamin75@gmail.com>
2025-11-15 15:10:54 +0200

    Added chart, metrics and logging (#2)
    * Added helm chart
    
    * Typo
    
    * Made objects namespaced and fixed port in dockerfile
    
    * Change exported port
    
    * Added prom monitoring stack
    
    * Added custom metrics
    
    * Added more logging and prom annotations for app
    
    * Added servicemonitor and dedicated healthcheck and livenesscheck route
    
    * Successful requests metric
    
    * Probes check health route
    
    * Add env variables in chart and grafana dashboard json
    
    * Added karpenter
    
    * Updated grafana dashboard

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Response
 import requests
 import os
 
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from logger import init_logger
+from metrics import *
 
 init_logger()
 logger = logging.getLogger()
@@ -12,26 +14,56 @@ logger = logging.getLogger()
 app = FastAPI()
 
 supported_currencies = ["usd", "ils"]
+supported_coins = ["bitcoin"]
 
 CURRENCY = os.getenv("API_CURRENCY", "usd").lower()
 if CURRENCY not in supported_currencies:
     logger.warning("Non-supported currency, defaulting to USD")
     CURRENCY = "usd"
 
+COIN_TYPE = os.getenv("API_COIN_TYPE", "bitcoin").lower()
+if COIN_TYPE not in supported_coins:
+    logger.warning("Non-supported coin, defaulting to bitcoin")
+    COIN_TYPE = "bitcoin"
+
 BASE_URL = os.getenv("API_URL", f"https://api.coingecko.com/api/v3/simple/price")
 
 @app.get("/price")
-def get_crypto_price(crypto: str = Query("bitcoin", description="Cryptocurrency name"),
-                     currency: str = Query("usd", description="Currency type")):
+def get_crypto_price(crypto: str = Query(COIN_TYPE, description="Cryptocurrency name"),
+                     currency: str = Query(CURRENCY, description="Currency type")):
     params = {
     "ids": crypto,
     "vs_currencies": currency
     }
-    response = requests.get(BASE_URL, params=params)
-    response.raise_for_status()
-    data = response.json()
+    start = time.time()
+    try:
+        response = requests.get(BASE_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Failed to fetch price: {e}")
+        increase_site_api_error(BASE_URL)
+        return {"error": "Failed to fetch price"}
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        increase_site_api_error(BASE_URL)
+        return {"error": "Unexpected error"}
 
     if crypto not in data:
+        increase_no_price(BASE_URL)
+        record_latency(BASE_URL, COIN_TYPE, start)
+        logger.error(f"Price not found for '{crypto}'")
         return {"error": f"Price not found for '{crypto}'"}
 
+    increase_successful_request_count(BASE_URL, crypto)
+    record_latency(BASE_URL, COIN_TYPE, start)
+    logger.info(f"Successfully fetched price for {crypto} from {BASE_URL}")
     return {"crypto": crypto, "price": data[crypto][currency.lower()]}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
